b87599a019c26b4962347ebfadd7c0ad71dee855
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2025-02-15 19:58:17 -0300

    Development of the common person registration form

--- src/views/componentsLogin/FormPersonCommon.vue
+++ src/views/componentsLogin/FormPersonCommon.vue
@@ -1,11 +1,204 @@
 <script setup lang="ts">
+import { defineProps } from 'vue';
+import { getImageUrl } from '../../utils/imageHelper'
+
+const props = defineProps<{
+    styleInputFocus: (event: Event) => void;
+}>();
 
 </script>
 
 <template>
+    <form id="containerFormCommon">
+        <section class="sections">
+            <aside class="asides">
+                <input type="text" id="Name" @keyup="props.styleInputFocus">
+                <label for="Name">Nome</label>
+                <div class="containerMensageError">
+                    <p>Preencha o campo!</p>
+                    <div class="box_iconAlert">
+                        <img :src="getImageUrl('iconAlert.png')" alt="">
+                    </div>
+                </div>
+            </aside>
 
+            <aside class="asides">
+                <input type="text" id="Surname" @keyup="props.styleInputFocus">
+                <label for="Surname">Sobrenome</label>
+                <div class="containerMensageError">
+                    <p>Preencha o campo!</p>
+                    <div class="box_iconAlert">
+                        <img :src="getImageUrl('iconAlert.png')" alt="">
+                    </div>
+                </div>
+            </aside>
+        </section>
+
+        <section class="sections"> 
+            <aside class="asides_full asides">
+                <input type="text" id="Cpf" @keyup="props.styleInputFocus">
+                <label for="Cpf">CPF</label>
+                <div class="containerMensageError">
+                    <p>Preencha o campo!</p>
+                    <div class="box_iconAlert">
+                        <img :src="getImageUrl('iconAlert.png')" alt="">
+                    </div>
+                </div>
+            </aside>
+        </section>
+
+        <section class="sections">
+            <aside class="asides">
+                <input type="text" id="Email" @keyup="props.styleInputFocus">
+                <label for="Email">Email</label>
+                <div class="containerMensageError">
+                    <p>Preencha o campo!</p>
+                    <div class="box_iconAlert">
+                        <img :src="getImageUrl('iconAlert.png')" alt="">
+                    </div>
+                </div>
+            </aside>
+
+            <aside class="asides">
+                <input type="text" id="ConfirmEmail" @keyup="props.styleInputFocus">
+                <label for="ConfirmEmail">Confirme seu email</label>
+                <div class="containerMensageError">
+                    <p>Preencha o campo!</p>
+                    <div class="box_iconAlert">
+                        <img :src="getImageUrl('iconAlert.png')" alt="">
+                    </div>
+                </div>
+            </aside>
+        </section>
+
+
+        <section class="sections">
+            <aside class="asides">
+                <input type="password" id="Password" @keyup="props.styleInputFocus">
+                <label for="Password">Senha</label>
+                <div class="containerMensageError">
+                    <p>Preencha o campo!</p>
+                    <div class="box_iconAlert">
+                        <img :src="getImageUrl('iconAlert.png')" alt="">
+                    </div>
+                </div>
+            </aside>
+
+            <aside class="asides asideButton">
+                <button id="buttonRegisterCommon">Cadastrar</button>
+            </aside>
+        </section>
+    </form>
 </template>
 
 <style scoped>
+#containerFormCommon {
+    /* border: 2px solid red; */
+    width: 100%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
 
+.sections {
+    /* border: 2px solid blue; */
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+}
+.asides {
+    width: 45%;
+    /* border: 2px solid green; */
+    display: flex;
+    flex-direction: column;
+    color: #ffffff;
+}
+.asides_full {
+    width: 100%;
+    /* border: 2px solid green; */
+    display: flex;
+    flex-direction: column;
+    color: #ffffff;
+}
+
+.asides > input {
+    border: 2px solid red;
+    width: 100%;
+    padding: 2px 5px;
+    background-color: transparent;
+    border: none;
+    border-bottom: 3px solid gray;
+    outline: none;
+    color: #ffffff;
+    font-size: 1.2vw;
+    transition: all 0.3s ease;
+    cursor: pointer;
+}
+
+.asides > input:focus {
+    border-bottom: 3px solid #ffffff;
+}
+.asides > label {
+    transform: translateY(-30px);
+    transition: all 0.3s ease-in-out;
+    font-size: 1.2vw;
+}
+.asides > input:focus + label {
+    transform: translateY(-55px);
+   
+}
+
+.containerMensageError {
+    display: none;
+    /* border: 1px solid red; */
+    transform: translateY(-10px);
+    align-items: center;
+    gap: 10px;
+}
+
+.containerMensageError > p {
+    color: rgb(245, 39, 39);
+    font-weight: bold;
+}
+
+.box_iconAlert {
+    width: 20px;
+    height: 20px;
+    /* border: 2px solid red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%; */
+}
+.box_iconAlert > img {
+    width: 100%;
+    height: 100%;
+}
+
+.classActiveFocus {
+    transform: translateY(-55px) !important;
+}
+.asideButton {
+    /* border: 1px solid red; */
+    display: flex;
+    align-items: flex-end;
+}
+#buttonRegisterCommon {
+    width: 50%;
+    padding: 10px 0;
+    background-color: #ffffff;
+    border: none;
+    border-radius: 7px;
+    color: #313131;
+    font-weight: 100;
+    cursor: pointer;
+    font-size: 1.1vw;
+    transition: all 0.3s ease;
+}
+
+#buttonRegisterCommon:hover {
+    scale: 1.1;
+}
 </style>
--- src/views/componentsLogin/Register.vue
+++ src/views/componentsLogin/Register.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { getImageUrl } from '../../utils/imageHelper'
 import { useRouter } from 'vue-router'
+
+import FormPersonCommon from './FormPersonCommon.vue'
+import FormPersonSeller from './FormPersonSeller.vue'
 
 const router = useRouter()
 
 function RouterLogin() {
     router.push('/')
+}
+
+const ShowFormPersonSeller = ref(false);
+function toggleForm(): void {
+    ShowFormPersonSeller.value = !ShowFormPersonSeller.value
+}
+
+function styleInputFocus(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    const label = document.querySelector(`label[for="${input.id}"]`) as HTMLLabelElement
+
+    if (input.value.length > 0) {
+        label.classList.add('classActiveFocus')
+    } else {
+        label.classList.remove('classActiveFocus')
+    }
+
+    //FUNÇÃO PARA PERSISTIR O FOCO DO INPUT CASO TIVER ALGO ESCRITO NELE
+
 }
 </script>
 
@@ -18,8 +42,18 @@ function RouterLogin() {
 
             <h1 id="backHome" @click="RouterLogin">Voltar</h1>
         </section>
-        <section>
+        <section id="container_MainFormRegister">
+            <div id="containerGroupForms_register">
+                <div id="TopLocal_ContainerForm_Register">
+                    <h1 id="titleForm">Cadastro usuário comum</h1>
 
+                    <button id="buttonToggleForm" @click="toggleForm">Cadastro vendedor</button>
+                </div>
+
+                <FormPersonCommon v-if="!ShowFormPersonSeller" :styleInputFocus="styleInputFocus"/>                
+                <FormPersonSeller v-else/>
+
+            </div>
         </section>
     </div>
 </template>
@@ -27,13 +61,16 @@ function RouterLogin() {
 <style scoped>
 #containerRegister {
     width: 100vw;
-    height: 100vh;
+    min-height: 100vh;
     background-image: url('../../assets/image/backgrond_register.png');
     background-position: center;
     background-repeat: no-repeat;
+    background-attachment: fixed;
     background-size: cover;
     display: flex;
     flex-direction: column;
+    /* justify-content: space-between; */
+    gap: 100px;
 }
 
 #container_logo_back_Login {
@@ -55,4 +92,65 @@ function RouterLogin() {
 #container_logoRegister > img {
     width: 100%;
 }
+
+#container_MainFormRegister {
+    /* border: 2px solid rgb(74, 146, 68); */
+    width: 100%;
+    height: 85%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    /* margin-bottom: 100px; */
+}
+
+#containerGroupForms_register {
+    width: 80%;
+    height: 90%;
+    background-color: rgba(255, 255, 255, 0.089);
+    backdrop-filter: blur(10px);
+    border-radius: 15px;
+    padding: 50px 50px 100px 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+}
+
+#TopLocal_ContainerForm_Register {
+    width: 100%;
+    height: 15%;
+    min-height: 7rem;
+    /* border: 2px solid rgb(26, 119, 33); */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+#titleForm {
+    width: 50%;
+    color: #ffffff;
+    font-size: 3.5rem;
+    font-weight: 100;
+}
+
+#buttonToggleForm {
+    width: 20%;
+    height: 50px;
+    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 0.089);
+    backdrop-filter: blur(10px);
+    border: none;
+    cursor: pointer;
+    color: #ffffff;
+    font-size: 1vw;
+    transition: all 0.3s ease;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+#buttonToggleForm:hover {
+    background-color: rgba(255, 255, 255, 0.295);
+    backdrop-filter: blur(10px);
+    scale: 1.1;
+}
+
 </style>
